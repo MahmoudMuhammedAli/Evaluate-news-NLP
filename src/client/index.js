1667090b7865f56e136388c75b06caa79cd1a3d6
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-04-18 09:51:45 +0200

    webpack5

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,17 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
 
-alert("I EXIST")
-console.log("CHANGE!!");
+console.log(checkForName); 
+
+
+
+export {
+    checkForName,
+    handleSubmit
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,31 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
 
 const app = express()
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+console.log(JSON.stringify(mockAPIResponse))
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    res.sendFile('dist/index.html')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+    res.json(mockAPIResponse);
+})
+
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
